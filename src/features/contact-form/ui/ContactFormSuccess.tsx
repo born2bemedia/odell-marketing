85f44d0f58b9a70d4705ca91c44ac70bb1618374
SergWebAcceptance <serg.web@acceptance.ltd>
2026-01-26 13:14:07 +0200

    Refactor ContactForm UI: update styles for improved layout and responsiveness, enhance success message with detailed feedback, and adjust button text for clarity.

--- src/features/contact-form/ui/ContactFormSuccess.tsx
+++ src/features/contact-form/ui/ContactFormSuccess.tsx
@@ -4,10 +4,11 @@ export const ContactFormSuccess = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className={styles.contactFormSuccess}>
       <div className={styles.contactFormSuccess__content}>
-        <h2>Message received</h2>
+        <h2>Thank you </h2>
+        <p>Thank you for contacting me and for taking the time to outline your situation in detail. I appreciate the context you’ve shared.</p>
         <span onClick={onClose}>
           <button type="button" className={styles.button}>
-            Continue
+          Return to home page
           </button>
         </span>
       </div>
